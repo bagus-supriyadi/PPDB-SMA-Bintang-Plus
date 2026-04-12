0982c08ee7b68802de06bf651386d6acdde9f5c7
bagus-supriyadi <bagus.supriyadi.bdl@gmail.com>
2026-04-12 09:00:12 +0700

    Update api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 // Service to interact with Google Apps Script Backend
 
 // To use the real backend, replace this URL with your deployed Google Apps Script Web App URL
-const GAS_WEB_APP_URL = ""; 
+const GAS_WEB_APP_URL = "https://script.google.com/macros/s/AKfycbzjCgJii9W3QpD2Zg943WtFgBaczKfhYrOQ-CBOvreMHeLYcxjId7hUCCc2UCin-Fyp/exec"; 
 
 export interface FormField {
   id: string;
